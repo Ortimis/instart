7d926ca0d5820783bf6afbe3393b38865f1c3315
Ortimis <jakob@drortmann.de>
2018-02-15 17:43:50 +0100

    c

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const firebase = {
+    apiKey: "AIzaSyCC8eX5QVgzkDH1-_tdg623YByoG0jpVMM",
+    authDomain: "instart-b473c.firebaseapp.com",
+    databaseURL: "https://instart-b473c.firebaseio.com",
+    projectId: "instart-b473c",
+    storageBucket: "instart-b473c.appspot.com",
+    messagingSenderId: "409664981767"
+  }
+  
+  export default { firebase }
